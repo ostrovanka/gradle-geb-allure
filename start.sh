36f98f58b311d18786d6140061268185c90af8ba
Anna Ostrovska <anna.ostrovska@care.com>
2021-01-18 18:00:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./gradlew testParam -DTEST_BROWSER=$1
-exec rm -rf /temp/* && cp /home/gradle/build/reports/allure-report /temp
+exec rm -rf /temp/* && cp /home/gradle/build/reports/allure-report /temp && ls
