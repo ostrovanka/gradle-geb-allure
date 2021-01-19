3de4d454432a923d2fbf6f8a80c1640acd894942
Anna Ostrovska <anna.ostrovska@care.com>
2021-01-19 11:38:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+report_dir=$(date +'%Y_%m_%d_%H-%m-%S')
+
 ## execute tests
 ./gradlew testParam -DTEST_BROWSER=$1
 
 ## copy allure report
-exec cp -r /home/gradle/build/reports/allure-report /temp/"$(date +'%m/%d/%Y/%H-%m-%S')"
+mkdir -p /temp/"$(report_dir)" && cp -r /home/gradle/build/reports/allure-report /temp/"$(report_dir)"
